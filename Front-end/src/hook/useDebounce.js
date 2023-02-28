2283850cf479179f5f0f2643cc4315f4b8adc21a
cuonglevan23 <cle17170@gmail.com>
2023-02-28 08:13:23 +0700

    update nav and useDebounce

---
+++ Front-end/src/hook/useDebounce.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+const useDebounce = () => {
+    const [debounceValue, setDebounceValue] = useState(value);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setDebounceValue(value);
+        }, delay);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [value, delay]);
+    return debounceValue;
+
+};
+
+
+
+export default useDebounce;
